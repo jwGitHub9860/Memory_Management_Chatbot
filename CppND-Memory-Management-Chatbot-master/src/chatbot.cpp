3859f6293629ca6f8432ebd4804a62ab3e0080c4
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-10-17 19:21:18 -0400

    Added whitespace to copy assignment operator

--- CppND-Memory-Management-Chatbot-master/src/chatbot.cpp
+++ CppND-Memory-Management-Chatbot-master/src/chatbot.cpp
@@ -61,9 +61,11 @@ ChatBot &operator=(const ChatBot &source);      // 3 : copy assignment operator
         return *this;
     }
     delete[] _rootNode; // deletes "_rootNode" since code looped before & need to deallocate data
+
     _chatLogic = source._chatLogic;   // creates copy of "chatLogic" from source
     _rootNode = new string[_chatLogic];     // dynamically allocates its own memory
     *_rootNode = *source._rootNode;     // copies content of sour to newly allocated heap memory (Deep Copy)
+    
     return *this;
 }
 ChatBot(ChatBot &&source);      // 4 : move constructor
